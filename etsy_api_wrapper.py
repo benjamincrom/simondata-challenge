4c75513a1fb2f960013158c889bdaba9a11b7964
Benjamin Crom <benjamincrom@gmail.com>
2016-01-19 15:13:39 -0500

    Updated etsy api wrapper

--- etsy_api_wrapper.py
+++ etsy_api_wrapper.py
@@ -2,20 +2,20 @@
 etsy_api_wrapper.py -- Wrap Etsy API functions so that when given a list of
                        stores, all listing titles and listing descriptions are
                        fetched for each store.
+
+                       How to run this module from command line:
+                       $ python etsy_api_wrapper.py [SHOP_ID]
 """
+import json
 import os
+import sys
 import requests
 
-# Helper functions
-def raise_value_error(message=''):
-    """ Throws ValueError """
-    raise ValueError(message)
 
 class EtsyAPIWrapper(object):
     """ See module docstring """
     ETSY_API_URL_ROOT = 'https://openapi.etsy.com/v2/'
-    ETSY_API_KEY = (os.environ.get('ETSY_API_KEY') or
-                    raise_value_error('ETSY_API_KEY not found in environment'))
+    ETSY_API_KEY = os.environ['ETSY_API_KEY']
 
     @classmethod
     def format_url_and_params(cls, shop_id, offset=0):
@@ -55,11 +55,17 @@ class EtsyAPIWrapper(object):
         list of titles and descriptions of all items for that shop_id.
         This method will automatically paginate with 100 items per request.
         """
-        response_dict = cls.execute_api_query(shop_id)
-        results_list = response_dict['results']
-        for offset in range(100, response_dict['count'], 100):
+        first_response_dict = cls.execute_api_query(shop_id)
+        results_list = first_response_dict['results']
+        for offset in range(100, first_response_dict['count'], 100):
             # Paginate with 100 items/page and update dictionary
             this_response_dict = cls.execute_api_query(shop_id, offset)
             results_list += this_response_dict['results']
 
         return results_list
+
+
+if __name__ == "__main__":
+    print json.dumps(
+        EtsyAPIWrapper().fetch_listing_titles_and_desc(sys.argv[1])
+    )
