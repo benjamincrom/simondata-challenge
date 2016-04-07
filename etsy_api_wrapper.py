9a552f3bf76d7fde49835112a22654931ba57238
Benjamin Crom <benjamincrom@gmail.com>
2016-04-07 17:56:09 -0400

    formatting

--- etsy_api_wrapper.py
+++ etsy_api_wrapper.py
@@ -1,11 +1,11 @@
-"""
+'''
 etsy_api_wrapper.py -- Wrap Etsy API functions so that when given a list of
                        stores, all listing titles and listing descriptions are
                        fetched for each store.
 
                        How to run this module from command line:
                        $ python etsy_api_wrapper.py [SHOP_ID]
-"""
+'''
 import os
 import time
 import requests
@@ -15,10 +15,10 @@ ETSY_API_KEY = os.environ['ETSY_API_KEY']
 QUERY_RETRIES = 2
 
 def format_url_and_params(shop_id, offset=0):
-    """
+    '''
     Given a shop_id and offset, use the already defined url root and API key
     to construct a target URL and dictionary of query parameters.
-    """
+    '''
     target_api_url = "{}/shops/{}/listings/active".format(ETSY_API_URL_ROOT,
                                                           shop_id)
 
@@ -32,9 +32,9 @@ def format_url_and_params(shop_id, offset=0):
     return (target_api_url, query_parameters)
 
 def execute_api_query(shop_id, offset=0):
-    """
+    '''
     Execute a single API query and return the json response dictionary
-    """
+    '''
     (target_api_url, query_parameters) = format_url_and_params(shop_id, offset)
     response = requests.get(target_api_url, params=query_parameters)
     remaining_retries = QUERY_RETRIES
@@ -48,11 +48,11 @@ def execute_api_query(shop_id, offset=0):
     return response.json()
 
 def fetch_listing_info(shop_id):
-    """
+    '''
     Fetch all listings from a given shop.  Given a shop_id, return
     list of titles and descriptions of all items for that shop_id.
     This method will automatically paginate with 100 items per request.
-    """
+    '''
     first_response_dict = execute_api_query(shop_id)
     results_list = first_response_dict['results']
     for offset in range(100, first_response_dict['count'], 100):
--- word_counter.py
+++ word_counter.py
@@ -1,4 +1,4 @@
-"""
+'''
 word_counter.py -- Given a list of formatted dictionaries from the
                    etsy_api_wrapper module, count the occurrences of all
                    words in each dictionary, then sum these counts together.
@@ -6,7 +6,7 @@ word_counter.py -- Given a list of formatted dictionaries from the
 
                    Note: uses the multiprocessing library for parallel
                    execution
-"""
+'''
 import re
 import multiprocessing
 from collections import Counter
@@ -20,7 +20,7 @@ IGNORE_LIST = ['to', 'are', 'the', 'and', 'in', 'is', 'if', 'your', 'my',
                'of', 'so', 'it', 'http', 'https', 'com', 'www', 'i', 'we']
 
 def remove_common_words(input_counter):
-    """ Removes common words and digits from a Counter """
+    ''' Removes common words and digits from a Counter '''
     for word in list(input_counter):
         if word in IGNORE_LIST or word.isdigit():
             del input_counter[word]
@@ -28,16 +28,16 @@ def remove_common_words(input_counter):
     return input_counter
 
 def count_words_in_str(input_str):
-    """ Breaks string into list of words then loads them into a Counter """
+    ''' Breaks string into list of words then loads them into a Counter '''
     word_list = WORD_SEARCH_REGEX.findall(input_str.lower())
     return Counter(word_list)
 
 def count_words_in_results_dict(input_dict):
-    """
+    '''
     Combine the two string values from an etsy_api_wrapper dictionary into a
     single string, then break the string down into a list of words and load
     them into a Counter.
-    """
+    '''
     combined_str = "{} {}".format(
         input_dict['title'].encode('ascii', 'ignore'),
         input_dict['description'].encode('ascii', 'ignore')
@@ -46,10 +46,10 @@ def count_words_in_results_dict(input_dict):
     return count_words_in_str(combined_str)
 
 def pool_handler(input_dict_list):
-    """
+    '''
     Create a multiprocessing pool and have it process the list of dictionaries
     from etsy_api_wrapper.  Remove the common words from the returned Counter.
-    """
+    '''
     process_pool = multiprocessing.Pool(DEFAULT_PROCESS_POOL_SIZE)
     counter_list = process_pool.map(count_words_in_results_dict,
                                     input_dict_list)
